076621e79add28c7c778742d0a3d11ec7240cf75
roeldev <roeldev@users.noreply.github.com>
2019-03-31 22:43:44 +0200

    feat: Added check for possible missing composer autoloader

--- youless/youless.php
+++ youless/youless.php
@@ -1,6 +1,11 @@
 <?php declare(strict_types=1);
 
-while(true) {
+$autoloader = __DIR__ . '/vendor/autoload.php';
+if (!file_exists($autoloader)) {
+    require_once __DIR__ . '/vendor/autoload.php';
+}
+
+while (true) {
     sleep(1);
-    echo \time() . PHP_EOL;
+    echo time() . PHP_EOL;
 }
