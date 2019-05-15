a690dcd6cafd9ed1c7b9e4b80d87e593c8eced10
roeldev <roeldev@users.noreply.github.com>
2019-05-15 21:30:42 +0200

    refact: Renamed fetch command to update

--- youless/app/Commands/UpdateCommand.php
+++ youless/app/Commands/UpdateCommand.php
@@ -6,13 +6,13 @@ use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
-class FetchCommand extends Command
+class UpdateCommand extends Command
 {
-    protected static $defaultName = 'fetch';
+    protected static $defaultName = 'update';
 
     protected function configure()
     {
-        $this->setDescription('Fetch latest data from YouLess device');
+        $this->setDescription('Update latest data from YouLess device');
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
