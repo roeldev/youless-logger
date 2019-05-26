09bd55393bccf29c1bf949fab9e99966d72d7a64
roeldev <roeldev@users.noreply.github.com>
2019-05-26 20:32:13 +0200

    feat: Create database

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -9,4 +9,17 @@ then
         --optimize-autoloader
 fi
 
+if [[ ! -f /youless-logger/data/youless-logger.db ]]
+then
+    echo Creating sqlite database, please wait...
+    sqlite3 /youless-logger/data/youless-logger.db "\
+        CREATE TABLE data( \
+           id INT PRIMARY KEY NOT NULL, \
+           resolution CHAR(1) NOT NULL, \
+           unit VARCHAR(4) NOT NULL, \
+           value INT NOT NULL, \
+           date DATETIME NOT NULL \
+        );"
+fi
+
 exec "$@"
