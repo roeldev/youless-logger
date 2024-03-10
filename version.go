f83f0c3620284128507d9635a3d77676520d1129
roeldev <roeldev@users.noreply.github.com>
2024-03-10 17:52:14 +0100

    Add dummy version

---
+++ version.go
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, Roel Schut. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package youlesslogger
+
+// Version of the package and its apps.
+const Version = "0.0.1"
